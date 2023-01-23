bebd9b576afd74e4ff660bb5a5e8db60c5311810
Russell Frankpitt <75209707+rustyff12@users.noreply.github.com>
2023-01-23 23:58:32 +0100

    added newCard + css & js fix

--- js/index.js
+++ js/index.js
@@ -1,45 +1,47 @@
 let firstCard = 10;
-let secondCard = 11;
-
-//let firstCard = Math.floor(Math.random() *10) +2;
-//let secondCard = Math.floor(Math.random() *10) +2;
+let secondCard = 4;
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
-
-console.log(sum);
-
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂";
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳";
-    hasBlackJack = true;
-} else {
-    message = "You're out of the game! 😭";
-    isAlive = false;
+function startGame() {
+    renderGame();
 }
 
-// CASH OUT!
-console.log(hasBlackJack);
-console.log(isAlive);
-console.log(message);
+function renderGame() {
+    cardsEl.textContent = `Cards: ${cards[0]} ${cards[1]}`;
+    sumEl.textContent = `Sum: ${sum}`;
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?";
+    } else if (sum === 21) {
+        message = "You've got Blackjack!";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game!";
+        isAlive = false;
+    }
+    messageEl.textContent = message;
+    
 
-
-
-
-/*
-let age = 22
-
-if (age < 21) {
-    console.log("You can not enter the club!");
-} else {
-    console.log("Welcome!");
 }
-*/
+
+
+function newCard() {
+    let thirdCard = 6;
+    sum += thirdCard;  
+    startGame(); 
+}
 
 
 
 
 
+
+
+//let firstCard = Math.floor(Math.random() *10) +2;
+//let secondCard = Math.floor(Math.random() *10) +2;
